7a072ca1fc1e93e296ef40e5935087597fe95a5b
jasvo <jasvo@hotmail.com>
2022-08-04 21:03:41 -0300

    Firts Commit

---
+++ SalesMvcWeb/Data/SalesMvcWebContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMvcWeb.Models;
+
+namespace SalesMvcWeb.Data
+{
+    public class SalesMvcWebContext : DbContext
+    {
+        public SalesMvcWebContext (DbContextOptions<SalesMvcWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
